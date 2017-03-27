bbe70dd7f9c4e0857b211af92c378b5e48775ee3
Paraprax <parkerjohnston28@gmail.com>
2017-03-27 18:33:23 -0400

    added age calculator to program

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
  puts "What is your name?"
  user_input = gets.chomp
  puts "Hi, #{user_input}!"
+ puts "What year were you born?"
+ year = gets.chomp.to_i
+ #using .to_i to convert year variable to an integer 
+ puts "You are #{2017 - year} years old this year."
