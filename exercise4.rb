970140e3723ce06deff66038c91e8dd4e4553999
Paraprax <parkerjohnston28@gmail.com>
2017-03-27 19:26:34 -0400

    still not working....

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,12 @@
 one_to_hundred = (1..100)
+
 one_to_hundred.each do |num|
-  if num % 3 == 0 && num % 5 != 0
+  if (num % 3 == 0 && num % 5 != 0)
     puts "Bit"
-  elsif num % 5 == 0 && num % 3 != 0
+  elsif (num % 5 == 0 && num % 3 != 0)
     puts "Maker"
-  elsif num % 5 == 0 && num % 3 == 0
+  elsif (num % 5 == 0 && num % 3 == 0)
     puts "BitMaker"
   else
     puts num
-  end
+end
