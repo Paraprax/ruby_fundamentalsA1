63f58a6582480130bc642110cb9e879e0d385e00
Paraprax <parkerjohnston28@gmail.com>
2017-03-27 18:03:19 -0400

    solving four problems in exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# the tip should be about 18 percent of 55 dollars
+puts (55.00/100)*18
+
+# use .to_s to convert integers into being read as strings
+puts 10.to_s + "Parker"
+
+#calculation using string interpolation
+puts "A sum of #{45628 * 7839} dollars would be a lot of money."
+
+# the following expression will produce the value "true"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
