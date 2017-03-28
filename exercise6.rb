b0bc47e5d1e045a8dcb09e1c8d490c8588ee759b
Paraprax <parkerjohnston28@gmail.com>
2017-03-28 00:01:23 -0400

    finished initial program

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,7 @@
 counter = 0
 
+while counter >= 0
+
   puts "You are #{counter} km from home. Would you like to walk or run?"
 
     input = gets
@@ -7,13 +9,12 @@ counter = 0
 
       if input == "walk"
         counter += 1
-          puts "You are #{counter} km from home. Would you like to walk or run?"
+          
 
       elsif input == "run"
         counter += 6
-          puts "You are #{counter} km from home. Would you like to walk or run?"
+
       else
         puts "?"
       end
-
-    input = gets.chomp
+end
