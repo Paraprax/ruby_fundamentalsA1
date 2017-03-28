74e06ad7f8cfa2909c7a528dce50b294459c0ed8
Paraprax <parkerjohnston28@gmail.com>
2017-03-28 00:11:27 -0400

    finished distance and energy tracker version of program for exercise 6.2

--- exercise6.rb
+++ exercise6.rb
@@ -1,20 +1,33 @@
 counter = 0
+energy = 10
 
-while counter >= 0
+while energy > 0
 
-  puts "You are #{counter} km from home. Would you like to walk or run?"
+  puts "You are #{counter} km from home. Your energy is #{energy}. Would you like to walk or run?"
+
 
     input = gets
     input = input.chomp
 
       if input == "walk"
         counter += 1
-          
+        energy  += 1
 
       elsif input == "run"
         counter += 6
+        energy -= 2
+
+      elsif input == "go home"
+        puts " "
 
       else
         puts "?"
       end
+
+if energy == 0
+
+  puts "You are out of energy. Go home for a rest!"
+
+end
+
 end
