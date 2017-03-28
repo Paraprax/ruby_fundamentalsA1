6aab352416ee9240a3b6c10bcdabc1716c469524
Paraprax <parkerjohnston28@gmail.com>
2017-03-27 23:38:24 -0400

    added input query for toppings and order confirmation message

--- exercise5pizza.rb
+++ exercise5pizza.rb
@@ -1,3 +1,9 @@
 puts "Yo! How many pizzas today, mac?"
-quantity = gets.chomp
-puts "Alright, #{quantity} pizzas comin' right up."
+
+  quantity = gets.chomp
+
+puts "Alright, #{quantity} pizzas comin' right up. How many toppings would you like on each one?"
+
+  toppings = gets.chomp
+
+puts "Okay, #{quantity} pizzas with #{toppings} toppings each, on their way."
